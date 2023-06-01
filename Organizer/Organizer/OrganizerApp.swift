df74154705ac6c9667ab4d7b2a293cc5cb25a11c
Oliver Fobian <oliver.fobian@check24.de>
2023-06-01 13:08:34 +0200

    init commit

---
+++ Organizer/Organizer/OrganizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrganizerApp.swift
+//  Organizer
+//
+//  Created by Oliver Fobian on 01.06.23.
+//
+
+import SwiftUI
+
+@main
+struct OrganizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
