bb6526a403ba8f3a4c56b8ad29922be545458ec3
Oliver Fobian <oliver.fobian@check24.de>
2023-06-01 15:52:47 +0200

    Created a basic folder structure

--- Organizer/Organizer/ContentView.swift
+++ Organizer/Organizer/ContentView.swift
@@ -1,21 +1,8 @@
-//
-//  ContentView.swift
-//  Organizer
-//
-//  Created by Oliver Fobian on 01.06.23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        HomeView()
     }
 }
 
---
+++ Organizer/Organizer/Features/Home/UI/HomeView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("This is the Home View of the App")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
